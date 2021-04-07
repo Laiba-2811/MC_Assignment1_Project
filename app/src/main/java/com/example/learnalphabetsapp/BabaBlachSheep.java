6afb749057f4486108a075374337442233a3e579
mcsf19a002@pucit.edu.pk <mcsf19a002@pucit.edu.pk>
2021-04-07 23:20:43 +0500

    more activities added

---
+++ app/src/main/java/com/example/learnalphabetsapp/BabaBlachSheep.java
@@ -0,0 +1,14 @@
+package com.example.learnalphabetsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BabaBlachSheep extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baba_blach_sheep);
+    }
+}
---
+++ app/src/main/java/com/example/learnalphabetsapp/TwoLittle.java
@@ -0,0 +1,14 @@
+package com.example.learnalphabetsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwoLittle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_little);
+    }
+}
