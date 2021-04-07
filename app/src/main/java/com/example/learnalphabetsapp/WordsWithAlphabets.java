f4eefcda6a843f0463bc1d3505e1d308585a494f
mcsf19a002@pucit.edu.pk <mcsf19a002@pucit.edu.pk>
2021-04-07 17:15:16 +0500

    2 Activities added

---
+++ app/src/main/java/com/example/learnalphabetsapp/WordsWithAlphabets.java
@@ -0,0 +1,14 @@
+package com.example.learnalphabetsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WordsWithAlphabets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_words_with_alphabets);
+    }
+}
